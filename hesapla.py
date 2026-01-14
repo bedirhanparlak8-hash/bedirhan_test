008d9c2d1e0d2aac4fd508f185a5402e776f91c6
bedirhanparlak8-hash <bedirhanparlak8@gmail.com>
2026-01-14 15:11:03 +0300

    Güvenli Liman: Kodlar çalışıyor

--- hesapla.py
+++ hesapla.py
@@ -9,3 +9,11 @@ def carp(a, b):
 
 print(f"4 * 3 sonucu: {carp(4, 3)}")
 print("Terminalden selamlar!")
+def bolme(a, b):
+    if b == 0:
+        return "Hata: Bir sayı 0'a bölünemez kanka!"
+    return a / b
+
+# Test etmek için:
+print("Bölme Sonucu:", bolme(10, 2))
+print("Sıfıra Bölme Testi:", bolme(10, 0))
