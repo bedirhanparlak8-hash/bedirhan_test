831857309d39b3a9a2baac81b857e60ff569f174
Yasin Şahin <56588137+yasinsahin0@users.noreply.github.com>
2026-01-14 13:17:28 +0300

    hesapla.py bilgisayardan güncellendi

--- hesapla.py
+++ hesapla.py
@@ -3,3 +3,9 @@ def topla(a, b):
 
 def cikar(a, b):
     return a - b
+# Mevcut fonksiyonların altına ekle
+def carp(a, b):
+    return a * b
+
+print(f"4 * 3 sonucu: {carp(4, 3)}")
+print("Terminalden selamlar!")
