cc8a31466a216bc5247654991a0097df33666241
bedirhanparlak8-hash <bedirhanparlak8@gmail.com>
2026-01-14 12:28:10 +0300

    Update topla and cikar functions in hesapla.py
    Modified the topla function to add 1 to the sum and adjusted the cikar function.

--- hesapla.py
+++ hesapla.py
@@ -1,8 +1,5 @@
 def topla(a, b):
-    return a + b
+    return a + b + 1  # İşte buraya +1 ekleyerek kodu bozuyoruz
 
 def cikar(a, b):
     return a - b
-
-if __name__ == "__main__":
-    print(f"5 + 3 sonucu: {topla(5, 3)}")
