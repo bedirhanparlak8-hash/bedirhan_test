40ea748711893c75c8914649d28b6a3e40c0e043
bedirhanparlak8-hash <bedirhanparlak8@gmail.com>
2026-01-14 12:28:10 +0300

    Update topla and cikar functions in hesapla.py
    Modified the topla function to add 1 to the sum and adjusted the cikar function.

--- hesapla.py
+++ hesapla.py
@@ -1,5 +1,5 @@
 def topla(a, b):
-    return a + b
+    return a + b + 1  # İşte buraya +1 ekleyerek kodu bozuyoruz
 
 def cikar(a, b):
     return a - b
@@ -8,7 +8,7 @@ def carp(a, b):
     return a * b
 
 print(f"4 * 3 sonucu: {carp(4, 3)}")
-print("Terminalden selamlar!")
+
 def bolme(a, b):
     if b == 0:
         return "Hata: Bir sayı 0'a bölünemez kanka!"
@@ -18,7 +18,9 @@ def bolme(a, b):
 print("Bölme Sonucu:", bolme(10, 2))
 print("Sıfıra Bölme Testi:", bolme(10, 0))
 
-=======
+
+
 #öyle böyle bir şey
 #sldlsl
+
 
